ff59a9e15e1b83e457e47fbb7e15f9d61f11f313
Diogo Silva <73388219+clebinho1234@users.noreply.github.com>
2024-03-07 13:21:06 +0000

    Update index.js

--- index.js
+++ index.js
@@ -105,6 +105,8 @@ app.post('/api/persons', (request, response) => {
 })
   
 const PORT = process.env.PORT || 3001
-  app.listen(PORT, () => {
+const currentUrl = window.location.href;
+console.log(currentUrl);
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
