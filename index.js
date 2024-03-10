cd8f2fa05a38e74476bf50cf4254b4038ad1d41c
Diogo Silva <73388219+clebinho1234@users.noreply.github.com>
2024-03-10 11:44:53 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+const Person = require('./models/person')
+
+/*
 let persons = [
     { 
         "id": 1,
@@ -24,6 +28,7 @@ let persons = [
         "number": "39-23-6423122"
       }
 ]
+*/
 
 app.use(express.static('dist'))
 
@@ -45,7 +50,9 @@ const unknownEndpoint = (request, response) => {
 }
   
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/info', (request, response) => {
@@ -55,62 +62,74 @@ app.get('/info', (request, response) => {
   response.send(info)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.statusMessage = "Person not found";
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id).then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-  
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.name) {
+  if (body.name === '') {
     return response.status(400).json({ 
       error: 'name missing' 
     })
   }
 
-  if (!body.number) {
+  if (body.number === '') {
     return response.status(400).json({ 
       error: 'number missing' 
     })
   }
 
-  let nameExists = persons.some(person => person.name === body.name)
+  /*let nameExists = persons.some(person => person.name === body.name)
 
   if(nameExists){
     return response.status(400).json({ 
       error: 'name already exists' 
     })
-  }
+  }*/
     
-  const id = Math.floor(Math.random() * 100 + 1)
-  const person = {
+  //const id = Math.floor(Math.random() * 100 + 1)
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: id,
-  }
+  })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
   
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3] || null
+const number = process.argv[4] || null
+
+const url =
+  `mongodb+srv://diogosilva2704:${password}@cluster0.lowevny.mongodb.net/personApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', noteSchema)
+
+if(name === null || number === null){
+  console.log('phonebook:');
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+} else{
+  const person = new Person({
+    name: name,
+    number: number
+  })
+
+  person.save().then(result => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
